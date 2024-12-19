9bbc5c6c81a31b0dc37033e3720859fa2db601ac
Husnainbot786 <113685920+Husnainbot786@users.noreply.github.com>
2024-12-19 07:20:56 +0500

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&adkyhDBL#xZp5VjYF7s2EwHoaEunha23smAGXNH8KqmkS9oI32KQ",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&OYlzAKQK#DnbPGxm1MRtOixT3rrerLMmN5mHDiI-fnQHWFj5ggGU",
 };
